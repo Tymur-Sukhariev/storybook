bdc01412b8436819559234f69d4b0b17c6b6326e
timur_sukhariev <payeer931@gmail.com>
2025-07-17 18:55:19 +0300

    Fix/Updated version installation

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,13 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@chromatic-com/storybook',
     '@storybook/addon-docs',
     '@storybook/addon-onboarding',
     '@storybook/addon-a11y',
-    '@storybook/addon-vitest',
-    '@storybook/addon-controls',
-    '@storybook/addon-actions',
-    '@storybook/addon-essentials',
+    '@storybook/addon-vitest'
   ],
   framework: {
     name: '@storybook/react-vite',
